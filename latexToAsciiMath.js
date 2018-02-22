06a70d7529a3dfc0768b5474d740d210cda14c32
Alfredo <gonzalezalfredo101@gmail.com>
2018-02-22 12:15:51 -0800

    first working commit. Very basic

--- latexToAsciiMath.js
+++ latexToAsciiMath.js
@@ -8,170 +8,166 @@ window.latexToAsciiMath = function (latexString) {
     let originalString = latexString;
     var asciiString = '';
     
-    //while(latexString.length) {
-        // find next command
-        let cmdIndex = latexString.search(/[\\^_].?/);
-        if (cmdIndex < 0) {
-            asciiString += latexString;
-            //break;
-        }
-        asciiString += latexString.slice(0, cmdIndex);
-        latexString = latexString.substr(cmdIndex).trim();
+    // find next command
+    let cmdIndex = latexString.search(/[\\^_].?/);
+    if (cmdIndex < 0) {
+        return asciiString + latexString;
+    }
+    asciiString += latexString.slice(0, cmdIndex);
+    latexString = latexString.substr(cmdIndex).trim();
 
 
-        // parse command
-        let cmdEndIndex = latexString.search(/[\ \\\(){+-=_^]/);
-        let cmd = latexString.slice(0, cmdEndIndex);
+    // parse command
+    let cmdEndIndex = latexString.search(/[\ \\\(){+-=_^]/);
+    let cmd = latexString.slice(0, cmdEndIndex);
 
-        if (latexString.substr(0,1) == '^' || latexString.substr(0,1) == '_') {
-            if (isNaN(latexString.substr(1,1)) == false) {
-                asciiString += cmd;
-                latexString = latexString.substr(cmdEndIndex);
-                // continue;
-            } else {
-                cmd = latexString.substr(0,1);
-                latexString = latexString.substr(1).trim();
-                console.log(cmd);
-            }
-        } else {
-            latexString = latexString.substr(1).trim();
-            cmdEndIndex = latexString.search(/[\ \\\(){+-=_^]/);
-            cmd = latexString.slice(0, cmdEndIndex).trim();
-        }
+    if (latexString.substr(0,1) == '^' || latexString.substr(0,1) == '_') {
+        cmd = latexString.substr(0,1);
+        asciiString += cmd;
+        latexString = latexString.substr(1).trim();
+    } else {
+        latexString = latexString.substr(1).trim();
+        cmdEndIndex = latexString.search(/[\ \\\(){+-=_^]/);
+        cmd = latexString.slice(0, cmdEndIndex).trim();
+    }
 
-        var ipsa = [];
-        switch(cmd) {
-            // followed by single {}
-            case '^':
-            case '_':
-                ipsa = getInnerParenString(latexString, '{');
-                if (ipsa[1] > 1) { asciiString += '('}
-                asciiString += latexToAsciiMath(ipsa[0]);
-                if (ipsa[1] > 1) { asciiString += ')'}
-                latexString = latexString.substr(ipsa[1] + 2).trim();
-                break;
+    var ipsa = [];
+    switch(cmd) {
+        // followed by single {}
+        case '^':
+        case '_':
+            if (latexString.substr(0, 1) != '{') {break};
+
+            ipsa = getInnerParenString(latexString, '{');
+            if (ipsa[1] > 1) { asciiString += '('}
+            asciiString += latexToAsciiMath(ipsa[0]);
+            if (ipsa[1] > 1) { asciiString += ')'}
+            latexString = latexString.substr(ipsa[1] + 2).trim();
+            break;
 
 
-            case 'frac':
-            // followed by by {}{}
-                console.log('fraction');
-                latexString = latexString.substr(cmd.length).trim();
+        // followed by by {}{}
+        case 'frac':
+            latexString = latexString.substr(cmd.length).trim();
 
-                ipsa = getInnerParenString(latexString, '{');
-                if (ipsa[1] > 1) { asciiString += '('}
-                asciiString += latexToAsciiMath(ipsa[0]);
-                if (ipsa[1] > 1) { asciiString += ')'}
-                latexString = latexString.substr(ipsa[1] + 2).trim();
+            ipsa = getInnerParenString(latexString, '{');
+            if (ipsa[1] > 1) { asciiString += '('}
+            asciiString += latexToAsciiMath(ipsa[0]);
+            if (ipsa[1] > 1) { asciiString += ')'}
+            latexString = latexString.substr(ipsa[1] + 2).trim();
 
-                asciiString += '/';
+            asciiString += '/';
 
-                ipsa = getInnerParenString(latexString, '{');
-                console.log(ipsa);
-                if (ipsa[1] > 1) { asciiString += '('}
-                asciiString += latexToAsciiMath(ipsa[0]);
-                if (ipsa[1] > 1) { asciiString += ')'}
-                latexString = latexString.substr(ipsa[1] + 2).trim();
-                break;
+            ipsa = getInnerParenString(latexString, '{');
+            if (ipsa[1] > 1) { asciiString += '('}
+            asciiString += latexToAsciiMath(ipsa[0]);
+            if (ipsa[1] > 1) { asciiString += ')'}
+            latexString = latexString.substr(ipsa[1] + 2).trim();
+            break;
 
-            // sometimes followed by \left( and \right)
-            case 'arcsin':
-            case 'arccos':
-            case 'arctan':
-            case 'sin':
-            case 'cos':
-            case 'tan':
-            case 'arcsec':
-            case 'arccsc':
-            case 'arccot':
-            case 'sec':
-            case 'csc':
-            case 'cot':
-                console.log('function: ', cmd);
-                asciiString += cmd;
-                latexString = latexString.substr(cmd.length).trim();
-                break;
+        // followed by \left( and \right)
+        case 'sqrt':
+        case 'log':
+        case 'ln':
+        case 'arcsin':
+        case 'arccos':
+        case 'arctan':
+        case 'sin':
+        case 'cos':
+        case 'tan':
+        case 'arcsec':
+        case 'arccsc':
+        case 'arccot':
+        case 'sec':
+        case 'csc':
+        case 'cot':
+            asciiString += cmd; 
+            latexString = latexString.substr(cmd.length);
+            break;
 
-            case 'left':
-                asciiString += '(';
-                latexString = latexString.substr(cmd.length + 1).trim();
-                break;
-            case 'right':
-                asciiString += ')';
-                latexString = latexString.substr(cmd.length + 1).trim();
-                break;
+        case 'left':
+            asciiString += '(';
+            latexString = latexString.substr(cmd.length + 1).trim();
+            break;
+        case 'right':
+            asciiString += ')';
+            latexString = latexString.substr(cmd.length + 1).trim();
+            break;
 
-            case 'alpha':
-            case 'beta':
-            case 'gamma':
-            case 'delta':
-            case 'epsilon':
-            case 'zeta':
-            case 'eta':
-            case 'theta':
-            case 'iota':
-            case 'kappa':
-            case 'lambda':
-            case 'mu':
-            case 'nu':
-            case 'xi':
-            case 'omnicron':
-            case 'pi':
-            case 'rho':
-            case 'sigma':
-            case 'tau':
-            case 'upsilon':
-            case 'phi':
-            case 'chi':
-            case 'psi':
-            case 'omega':
-            case 'Alpha':
-            case 'Beta':
-            case 'Gamma':
-            case 'Delta':
-            case 'Epsilon':
-            case 'Zeta':
-            case 'Eta':
-            case 'Theta':
-            case 'Iota':
-            case 'Kappa':
-            case 'Lambda':
-            case 'Mu':
-            case 'Nu':
-            case 'Xi':
-            case 'Omnicron':
-            case 'Pi':
-            case 'Rho':
-            case 'Sigma':
-            case 'Tau':
-            case 'Upsilon':
-            case 'Phi':
-            case 'Chi':
-            case 'Psi':
-            case 'Omega':
-                console.log('hhhh');
-                asciiString += cmd;
-                latexString = latexString.substr(cmd.length).trim();
-                break;
-                
-        }
+        case 'alpha':
+        case 'beta':
+        case 'gamma':
+        case 'delta':
+        case 'epsilon':
+        case 'zeta':
+        case 'eta':
+        case 'theta':
+        case 'iota':
+        case 'kappa':
+        case 'lambda':
+        case 'mu':
+        case 'nu':
+        case 'xi':
+        case 'omnicron':
+        case 'pi':
+        case 'rho':
+        case 'sigma':
+        case 'tau':
+        case 'upsilon':
+        case 'phi':
+        case 'chi':
+        case 'psi':
+        case 'omega':
+        case 'Alpha':
+        case 'Beta':
+        case 'Gamma':
+        case 'Delta':
+        case 'Epsilon':
+        case 'Zeta':
+        case 'Eta':
+        case 'Theta':
+        case 'Iota':
+        case 'Kappa':
+        case 'Lambda':
+        case 'Mu':
+        case 'Nu':
+        case 'Xi':
+        case 'Omnicron':
+        case 'Pi':
+        case 'Rho':
+        case 'Sigma':
+        case 'Tau':
+        case 'Upsilon':
+        case 'Phi':
+        case 'Chi':
+        case 'Psi':
+        case 'Omega':
+            asciiString += cmd;
+            latexString = latexString.substr(cmd.length).trim();
+            break;
+            
+    }
 
+    // DEBUG LOGS
+    console.log('\n\n');
+    console.log('  org: ', originalString);
+    console.log('\n\n');
+    console.log('  cmd: ', cmdIndex);
+    console.log('  cmd: ', cmd);
+    console.log('ascii: ', asciiString);
+    console.log('rmtex: ', latexString);
 
-        console.log('\n\n');
-        console.log('  org: ', originalString);
-        console.log('\n\n');
-        console.log('  cmd: ', cmdIndex);
-        console.log('  cmd: ', cmd);
-        console.log('ascii: ', asciiString);
-        console.log('rmtex: ', latexString);
+    console.log('\n\n');
+    console.log('chRem:: ', latexString.length);
+    console.log('\n\n\n\n');
 
-        console.log('\n\n');
-        console.log('chRem:: ', latexString.length);
-
-        if (latexString.length > 0) {
-            latexToAsciiMath(latexString);
-        }
-        return asciiString;
-    //}
+    if (latexString.length == originalString.length) {
+        throw('Unable to parse LaTeX string.');
+        return;
+    } else if (latexString.length > 0) {
+        asciiString += latexToAsciiMath(latexString);
+    }
+    return asciiString;
 }
 
 
@@ -202,7 +198,6 @@ function getInnerParenString(str, openParen) {
         str = str.substr(1);
         nextOpenParenIndex = str.search(openParen);
         nextCloseParenIndex = str.search(closeParen);
-        console.log(str);
     } else if (nextCloseParenIndex < 0) {
         alert('Syntax Error: No matching close bracket in entry.');
         return;
